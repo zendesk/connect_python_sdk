4d43e0a5ccebfe8e3b9ac4332b75d2f78e670d4e
Travis Beauvais <tbeauvais@gmail.com>
2014-07-22 15:41:36 -0700

    Update URL and description

--- setup.py
+++ setup.py
@@ -10,11 +10,17 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'outbound'))
 import version
 
 long_description = '''
-Josh should write this
+Outbound sends automated email, SMS, phone calls and push notifications based on the actions users take or do not take in your app. The Outbound API has two components:
+
+Identify each of your users and their attributes using an identify API call.
+Track the actions that each user takes in your app using a track API call.
+Because every message is associated with a user (identify call) and a specific trigger action that a user took or should take (track call), Outbound is able to keep track of how each message affects user actions in your app. These calls also allow you to target campaigns and customize each message based on user data.
+
+Example: When a user in San Francisco(user attribute) does signup(event) but does not upload a picture(event) within 2 weeks, send them an email about how they'll benefit from uploading a picture.
 '''
 
 setup(
-    name='outbound-python',
+    name='outbound',
     version=version.VERSION,
     url='https://github.com/outboundio/lib-python',
     author='Travis Beauvais',
@@ -26,6 +32,6 @@ setup(
     install_requires=[
         'requests',
     ],
-    description='Josh should write this as well',
+    description='Outbound sends automated email, SMS, phone calls and push notifications based on the actions users take (or do not take) in your app.',
     long_description=long_description
 )
